e0484d9e06b4678acc251b531f0681e5a488502a
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2024-04-22 20:03:24 +0700

    Update routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,11 @@
 import ListRestaurant from '../views/pages/list-restaurant';
 import Detail from '../views/pages/detail';
-
+import Favorite from '../views/pages/favorite';
 const routes = {
   '/': ListRestaurant, // default page
   '/list-restaurant': ListRestaurant,
   
-  // '/favorite': Favorite,
+  '/favorite': Favorite,
   '/detail/:id': Detail,
 };
 
